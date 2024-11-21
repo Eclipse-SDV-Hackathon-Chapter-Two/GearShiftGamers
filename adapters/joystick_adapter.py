d05b18ee463bb04b163c6d7172277e68d59ce5b2
Muzi Xu <yueranl03@gmail.com>
2024-11-21 10:54:39 +0100

    Muri joystick push

--- adapters/joystick_adapter.py
+++ adapters/joystick_adapter.py
@@ -10,6 +10,7 @@ import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import socket
+from parameters import joy_name_left, joy_name_right, joy_name_up
 
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
@@ -30,6 +31,7 @@ databroker_host = '192.168.0.148'
 databroker_port = '55555'
 client = VSSClient(host=databroker_host, port=databroker_port)
 client.connect()
+value_to_send = 1
 
 def log(msg):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -44,21 +46,25 @@ try:
           if x_value - 13180 > 20:
             print("right")
             movement = "right"
+            client.set_current_values({joy_name_right: Datapoint(value_to_send)})
+          else:
+             client.set_current_values({joy_name_right: Datapoint(0)})
 
           if 13180 - x_value > 20:
             print("left")
             movement = "left"
+            client.set_current_values({joy_name_left: Datapoint(value_to_send)})
+          else:
+            client.set_current_values({joy_name_left: Datapoint(0)})
 
           if y_value - 13044 > 20:
             print("jump")
             movement = "jump"
+            client.set_current_values({joy_name_up: Datapoint(value_to_send)})
+          else:
+            client.set_current_values({joy_name_up: Datapoint(0)})
 
-          if movement != "":
-           # client_socket.sendall(movement.encode())
-            client.set_current_values({
-            'Movement.direction': Datapoint(movement),
-            })
-            log(f"Fedding movement as {movement}.")
+
           print(f"X: {x_value}, Y: {y_value}")
           time.sleep(0.1)
 except KeyboardInterrupt:
--- adapters/parameters.py
+++ adapters/parameters.py
@@ -1,3 +1,7 @@
 signal_name_left = 'Vehicle.Acceleration.Longitudinal'
 signal_name_right = 'Vehicle.Acceleration.Lateral'
 signal_name_up = 'Vehicle.Acceleration.Vertical'
+
+joy_name_left = 'Vehicle.AngularVelocity.Roll'
+joy_name_right = 'Vehicle.AngularVelocity.Pitch'
+joy_name_up = 'Vehicle.AngularVelocity.Yaw'
