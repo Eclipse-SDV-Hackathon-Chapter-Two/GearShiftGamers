02f193195ed2b31feca79ad6dfcfbc3b51fd3692
HPC <you@exam>
2024-11-21 20:44:36 +0100

    changes refactor for xbox

--- adapters/xbox_adapter.py
+++ adapters/xbox_adapter.py
@@ -8,10 +8,8 @@ import random
 
 try:
     from adapters.parameters import signal_name_left, signal_name_right, signal_name_up
-    from adapters.parameters import joy_name_left, joy_name_right, joy_name_up
 except:
     from parameters import signal_name_left, signal_name_right, signal_name_up
-    from parameters import joy_name_left, joy_name_right, joy_name_up
 
 # databroker_host = '127.0.0.1'
 databroker_host = '0.0.0.0'
@@ -27,45 +25,59 @@ class XboxController(object):
     def __init__(self):
         # Initialize Pygame
         pygame.init()
-
         pygame.joystick.init()
 
+        # Initialize Joystick
         self.joystick = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())][0]
         self.joystick.init()
 
+        self.client = None
+
+    def connect_to_databroker(self) -> None: 
         self.client = VSSClient(host=databroker_host, port=databroker_port)
         self.client.connect()
 
-    def main(self):
-        # main loop
+
+    def main(self) -> None:
+        
         while True:
             time.sleep(0.01)
-
             pygame.event.pump()
 
+            try:
+                if self.client is None:
+                    self.connect_to_databroker()
+                    print("connected to databroker")
 
+                axis_0 = self.joystick.get_axis(0)
+                axis_1 = self.joystick.get_axis(1)
+                rand_offset = random.uniform(0.0, 0.1)
 
-            # left
-            if self.joystick.get_axis(0) < -joystick_tolerance:
-                self.client.set_current_values({signal_name_left: Datapoint(value_to_send + random.uniform(0.0, 0.1))})
-                print("<-", time.time())
-            else:
-                self.client.set_current_values({signal_name_left: Datapoint(0)})
+                # left
+                if axis_0 < -joystick_tolerance:
+                    self.client.set_current_values({signal_name_left: Datapoint(value_to_send + rand_offset)})
+                    print("<-")
+                else:
+                    self.client.set_current_values({signal_name_left: Datapoint(0)})
 
-            # right
-            if self.joystick.get_axis(0) > joystick_tolerance:
-                self.client.set_current_values({signal_name_right: Datapoint(value_to_send + random.uniform(0.0, 0.1))})
-                print("->")
-            else:
-                self.client.set_current_values({signal_name_right: Datapoint(0)})
+                # right
+                if axis_0 > joystick_tolerance:
+                    self.client.set_current_values({signal_name_right: Datapoint(value_to_send + rand_offset)})
+                    print("->")
+                else:
+                    self.client.set_current_values({signal_name_right: Datapoint(0)})
 
-            # up
-            if self.joystick.get_axis(1) < -joystick_tolerance:
-                self.client.set_current_values({signal_name_up: Datapoint(value_to_send + random.uniform(0.0, 0.1)) })
-                print("^")
-            else:
-                self.client.set_current_values({signal_name_up: Datapoint(0)})
+                # up
+                if axis_1 < -joystick_tolerance:
+                    self.client.set_current_values({signal_name_up: Datapoint(value_to_send + rand_offset) })
+                    print("^")
+                else:
+                    self.client.set_current_values({signal_name_up: Datapoint(0)})
 
+            except Exception as e:
+                print("Kuksa Databroker is down...try to connect")
+                self.client = None
+                time.sleep(1)
 
 if __name__=="__main__":
     xbox = XboxController()
